aaf7503b1fa9877b48a860bc8837589a96670671
Imon <contact@imonweb.co.uk>
2023-07-14 22:24:24 +0100

    show more

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,6 @@
 import  { fetchCars } from '@/utils';
 import { fuels, yearsOfProduction } from '@/constants';
-import { CarCard, CustomFilter, Hero, SearchBar } from '@/components'
+import { CarCard, CustomFilter, Hero, SearchBar, ShowMore } from '@/components'
 import Image from 'next/image'
 
 export default async function Home({ searchParams }) {
@@ -38,6 +38,11 @@ export default async function Home({ searchParams }) {
                 <CarCard car={car} />)
               }
             </div>  
+
+            <ShowMore
+              pageNumber={(searchParams.limit || 10) / 10}
+              isNext={(searchParams.limit || 10) > allCars.length}
+            />
           </section>
         ) : (
           <div className='home__error-container'>
